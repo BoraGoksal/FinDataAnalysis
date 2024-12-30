8e77dcb35dcebcb70020b9da19f189927dc3123a
boragoksal <boragoksal@outlook.com>
2024-12-30 14:35:14 +0000

    Added more tickers and improved exploratory_analysis.ipynb

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -1,12 +1,25 @@
 import yfinance as yf
+from datetime import datetime
 import os
 
-def collect_stock_data(ticker, start_date, end_date, save_path = "data/raw/"):
+def collect_stock_data(tickers, start_date, end_date, save_path = "/Users/boragoksal/PycharmProjects/FinDataAnalysis/data/raw"):
     os.makedirs(save_path, exist_ok= True)
-    data = yf.download(ticker, start = start_date, end = end_date)
-    file_path = os.path.join(save_path, f"{ticker}.csv")
-    data.to_csv(file_path)
-    print(f"Data for {ticker} saved to {file_path}")
+
+    #Download data for all tickers
+    data = yf.download(tickers, start = start_date, end = end_date)
+
+    #save each ticker's data as a seperate CSV file in to data/raw/
+    for ticker in tickers:
+        data_ticker = data.xs(ticker, level=1, axis=1) #Data for each ticker
+        file_path = os.path.join(save_path, f"{ticker}.csv")
+        data_ticker.to_csv(file_path)
+
+        print(f"Data for {ticker} saved to {file_path}")
 
 if __name__ == "__main__":
-    collect_stock_data("AAPL", "2020-01-01", "2023-01-01")
+
+    tickers = ['AAPL', 'GOOGL', 'MSFT', 'NFLX'] #tickers - add more if needed
+
+    start = "2018-01-01"
+    end = datetime.now()
+    collect_stock_data(tickers, start, end) #collecting data for specified dates
